41db6bf47e2810001004c1516ea30dbe396509a1
jacquesuys <jaxuys@gmail.com>
2016-01-25 15:49:25 -0800

    no longer get pwnd

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,71 +1,89 @@
-$(document).ready(function() {
-  $('.update').on('click', function() {
-    var $str = '';
-    $.get('https://api.parse.com/1/classes/chatterbox', function(data) {
-      $.each(data.results, function(index, message) {
-        $str += '<div>' + message.text + '</div>';
-      });
+ var entityMap = {
+   "&": "&amp;",
+   "<": "&lt;",
+   ">": "&gt;",
+   '"': '&quot;',
+   "'": '&#39;',
+   "/": '&#x2F;'
+ };
+
+
+ var escapeHtml = function(string) {
+   return String(string).replace(/[&<>"'\/]/g, function(s) {
+     return entityMap[s];
+   });
+ };
+
+ $(document).ready(function() {
+   $('.update').on('click', function() {
+     var $str = '';
+     $.get('https://api.parse.com/1/classes/chatterbox', function(data) {
+       $.each(data.results, function(index, message) {
+
+         $str += '<div>' + escapeHtml(message.text) + '</div>';
+       });
       $('messages').html($str);
-    });
-  });
+     });
+   });
 
-  var newObj = {
-    username: 'gerritLovesGettingHacked',
-    text: 'lets hack gerrit',
-    roomname: '4chan'
-  };
+   var newObj = {
+     username: 'gerritLovesGettingHacked',
+     text: 'lets hack gerrit',
+     roomname: '4chan'
+   };
 
-  $('.submit').on('click', function() {
-    $.ajax({
-      // This is the url you should use to communicate with the parse API server.
-      url: 'https://api.parse.com/1/classes/chatterbox',
-      type: 'POST',
-      data: JSON.stringify(newObj),
-      contentType: 'application/json',
-      success: function(data) {
-        console.log(data);
-      },
-      error: function(data) {
-        // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-        console.error('chatterbox: Failed to send message');
-      }
-    });
-  });
-
-
-  $('.update').click();
-});
-
-var message = {
-  username: 'gerritLovesGettingHacked',
-  text: 'lets hack gerrit',
-  roomname: '4chan'
-};
-
-// $.ajax({
-//   // This is the url you should use to communicate with the parse API server.
-//   url: 'https://api.parse.com/1/classes/chatterbox',
-//   type: 'POST',
-//   data: JSON.stringify(message),
-//   contentType: 'application/json',
-//   success: function (data) {
-//     console.log(data);
-//   },
-//   error: function (data) {
-//     // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-//     console.error('chatterbox: Failed to send message');
-//   }
-// });
+   $('.submit').on('click', function() {
+     $.ajax({
+       // This is the url you should use to communicate with the parse API server.
+       url: 'https://api.parse.com/1/classes/chatterbox',
+       type: 'POST',
+       data: JSON.stringify(newObj),
+       dataType: 'json',
+       contentType: 'application/x-www-form-urlencoded;charset=ISO-8859-15',
+       success: function(data) {
+         console.log(data);
+       },
+       error: function(data) {
+         // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+         console.error('chatterbox: Failed to send message');
+       }
+     });
+   });
 
 
-// Please remove below.
+   $('.update').click();
+ });
 
-/*
-createdAt: "2016-01-25T19:56:01.627Z"
-objectId: "O9jxNOG3LT"
-opponents: Object
-roomname: "4chan"
-text: "trololo"
-updatedAt: "2016-01-25T19:56:01.627Z"
-username: "shawndrost"
-*/
+ var message = {
+   username: 'gerritLovesGettingHacked',
+   text: 'lets hack gerrit',
+   roomname: '4chan'
+ };
+
+ // $.ajax({
+ //   // This is the url you should use to communicate with the parse API server.
+ //   url: 'https://api.parse.com/1/classes/chatterbox',
+ //   type: 'POST',
+ //   data: JSON.stringify(message),
+ //   contentType: 'application/json',
+ //   success: function (data) {
+ //     console.log(data);
+ //   },
+ //   error: function (data) {
+ //     // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+ //     console.error('chatterbox: Failed to send message');
+ //   }
+ // });
+
+
+ // Please remove below.
+
+ /*
+ createdAt: "2016-01-25T19:56:01.627Z"
+ objectId: "O9jxNOG3LT"
+ opponents: Object
+ roomname: "4chan"
+ text: "trololo"
+ updatedAt: "2016-01-25T19:56:01.627Z"
+ username: "shawndrost"
+ */

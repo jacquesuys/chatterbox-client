d303a49352dfc863cbab7c7c788c74a742e767ba
jacquesuys <jaxuys@gmail.com>
2016-01-25 16:23:02 -0800

    Builds rooms into a select drop down.

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -6,7 +6,7 @@
    "'": '&#39;',
    "/": '&#x2F;'
  };
-
+var roomNames = {};
 
  var escapeHtml = function(string) {
    return String(string).replace(/[&<>"'\/]/g, function(s) {
@@ -14,22 +14,35 @@
    });
  };
 
+ var updateRoomNames = function(message) {
+ };
+
  $(document).ready(function() {
-   $('.update').on('click', function() {
-     var $str = '';
+
+  var $update = function(){
+    var $str = '', $options;
      $.get('https://api.parse.com/1/classes/chatterbox', function(data) {
        $.each(data.results, function(index, message) {
-
+         roomNames[message.roomname] = escapeHtml(message.roomname);
          $str += '<div>' + escapeHtml(message.text) + '</div>';
        });
       $('messages').html($str);
+      
+      $.each(roomNames, function(key, room){
+        $options += '<option>' + room + '</option>';
+      });
+
+      $('.rooms').html($options);
+
      });
-   });
+  };
+
+   $('.update').on('click', $update);
 
    var newObj = {
      username: 'gerritLovesGettingHacked',
      text: 'lets hack gerrit',
-     roomname: '4chan'
+     roomname: 'SharkTank'
    };
 
    $('.submit').on('click', function() {
@@ -41,7 +54,7 @@
        dataType: 'json',
        contentType: 'application/x-www-form-urlencoded;charset=ISO-8859-15',
        success: function(data) {
-         console.log(data);
+        $update();
        },
        error: function(data) {
          // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
@@ -50,6 +63,9 @@
      });
    });
 
+$('select').on('change', function() {
+  console.log($('option:selected').text());
+});
 
    $('.update').click();
  });
@@ -60,30 +76,25 @@
    roomname: '4chan'
  };
 
- // $.ajax({
- //   // This is the url you should use to communicate with the parse API server.
- //   url: 'https://api.parse.com/1/classes/chatterbox',
- //   type: 'POST',
- //   data: JSON.stringify(message),
- //   contentType: 'application/json',
- //   success: function (data) {
- //     console.log(data);
- //   },
- //   error: function (data) {
- //     // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
- //     console.error('chatterbox: Failed to send message');
- //   }
- // });
 
 
- // Please remove below.
 
- /*
- createdAt: "2016-01-25T19:56:01.627Z"
- objectId: "O9jxNOG3LT"
- opponents: Object
- roomname: "4chan"
- text: "trololo"
- updatedAt: "2016-01-25T19:56:01.627Z"
- username: "shawndrost"
- */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

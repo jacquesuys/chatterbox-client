671c3f701578d7fb4faa3766750c2d7df6f4efd6
jacquesuys <jaxuys@gmail.com>
2016-01-25 16:57:03 -0800

    Structured as App object, sending, fetching and init-ing.

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,3 +1,16 @@
+var app = {
+  server: 'https://api.parse.com/1/classes/chatterbox',
+  init: function() {
+    $(document).ready(initialize);
+  },
+  send: function(message) {
+    send(message);
+  },
+  fetch: function(){
+    $update();
+  }
+};
+
 var entityMap = {
   "&": "&amp;",
   "<": "&lt;",
@@ -22,60 +35,59 @@ var updateRoomNames = function(roomNames) {
   $('.rooms').html($options);
 };
 
-$(document).ready(function() {
+var initialize = function() {
 
-
-  var $update = function() {
-    var $str = '';
-    var roomNames = {};
-
-    var parseMessage = function(index, message) {
-      roomNames[message.roomname] = escapeHtml(message.roomname);
-      $str += '<div>' + escapeHtml(message.text) + '</div>';
-    };
-
-    $.get('https://api.parse.com/1/classes/chatterbox', function(data) {
-
-      $.each(data.results, parseMessage);
-
-      $('messages').html($str);
-
-      updateRoomNames(roomNames);
-    });
-  };
+  $update();
 
   $('.update').on('click', $update);
 
-  var newObj = {
-    username: 'gerritLovesGettingHacked',
-    text: 'lets hack gerrit',
-    roomname: 'SharkTank'
-  };
-
-  $('.submit').on('click', function() {
-    $.ajax({
-      // This is the url you should use to communicate with the parse API server.
-      url: 'https://api.parse.com/1/classes/chatterbox',
-      type: 'POST',
-      data: JSON.stringify(newObj),
-      dataType: 'json',
-      contentType: 'application/x-www-form-urlencoded;charset=ISO-8859-15',
-      success: function(data) {
-        $update();
-      },
-      error: function(data) {
-        // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-        console.error('chatterbox: Failed to send message');
-      }
-    });
-  });
 
   $('select').on('change', function() {
-    console.log($('option:selected').text());
+    // console.log($('option:selected').text());
   });
 
   $('.update').click();
-});
+};
+
+var $update = function() {
+  var $str = '';
+  var roomNames = {};
+
+  var parseMessage = function(index, message) {
+    roomNames[message.roomname] = escapeHtml(message.roomname);
+    $str += '<div>' + escapeHtml(message.text) + '</div>';
+  };
+
+  $.get(app.server, function(data) {
+
+    $.each(data.results, parseMessage);
+
+    $('messages').html($str);
+
+    updateRoomNames(roomNames);
+  });
+};
+
+var send = function(messageObj) {
+  console.log(messageObj);
+
+  $.ajax({
+    // This is the url you should use to communicate with the parse API server.
+    url: 'https://api.parse.com/1/classes/chatterbox',
+    type: 'POST',
+    data: JSON.stringify(messageObj),
+    dataType: 'json',
+    contentType: 'application/x-www-form-urlencoded;charset=ISO-8859-15',
+    success: function(data) {
+      // $update();
+      return data;
+    },
+    error: function(data) {
+      // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to send message');
+    }
+  });
+};
 
 var message = {
   username: 'gerritLovesGettingHacked',

8660e6b074d27d8cb64818b5f406113bbc587b6d
jacquesuys <jaxuys@gmail.com>
2016-01-25 16:35:23 -0800

    Clean code

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,100 +1,84 @@
- var entityMap = {
-   "&": "&amp;",
-   "<": "&lt;",
-   ">": "&gt;",
-   '"': '&quot;',
-   "'": '&#39;',
-   "/": '&#x2F;'
- };
-var roomNames = {};
+var entityMap = {
+  "&": "&amp;",
+  "<": "&lt;",
+  ">": "&gt;",
+  '"': '&quot;',
+  "'": '&#39;',
+  "/": '&#x2F;'
+};
 
- var escapeHtml = function(string) {
-   return String(string).replace(/[&<>"'\/]/g, function(s) {
-     return entityMap[s];
-   });
- };
+var escapeHtml = function(string) {
+  return String(string).replace(/[&<>"'\/]/g, function(s) {
+    return entityMap[s];
+  });
+};
 
- var updateRoomNames = function(message) {
- };
+var updateRoomNames = function(roomNames) {
+  var $options = '';
+  $.each(roomNames, function(key, room) {
+    $options += '<option>' + room + '</option>';
+  });
 
- $(document).ready(function() {
+  $('.rooms').html($options);
+};
 
-  var $update = function(){
-    var $str = '', $options;
-     $.get('https://api.parse.com/1/classes/chatterbox', function(data) {
-       $.each(data.results, function(index, message) {
-         roomNames[message.roomname] = escapeHtml(message.roomname);
-         $str += '<div>' + escapeHtml(message.text) + '</div>';
-       });
+$(document).ready(function() {
+
+
+  var $update = function() {
+    var $str = '';
+    var roomNames = {};
+
+    var parseMessage = function(index, message) {
+      roomNames[message.roomname] = escapeHtml(message.roomname);
+      $str += '<div>' + escapeHtml(message.text) + '</div>';
+    };
+
+    $.get('https://api.parse.com/1/classes/chatterbox', function(data) {
+
+      $.each(data.results, parseMessage);
+
       $('messages').html($str);
-      
-      $.each(roomNames, function(key, room){
-        $options += '<option>' + room + '</option>';
-      });
 
-      $('.rooms').html($options);
-
-     });
+      updateRoomNames(roomNames);
+    });
   };
 
-   $('.update').on('click', $update);
+  $('.update').on('click', $update);
 
-   var newObj = {
-     username: 'gerritLovesGettingHacked',
-     text: 'lets hack gerrit',
-     roomname: 'SharkTank'
-   };
+  var newObj = {
+    username: 'gerritLovesGettingHacked',
+    text: 'lets hack gerrit',
+    roomname: 'SharkTank'
+  };
 
-   $('.submit').on('click', function() {
-     $.ajax({
-       // This is the url you should use to communicate with the parse API server.
-       url: 'https://api.parse.com/1/classes/chatterbox',
-       type: 'POST',
-       data: JSON.stringify(newObj),
-       dataType: 'json',
-       contentType: 'application/x-www-form-urlencoded;charset=ISO-8859-15',
-       success: function(data) {
+  $('.submit').on('click', function() {
+    $.ajax({
+      // This is the url you should use to communicate with the parse API server.
+      url: 'https://api.parse.com/1/classes/chatterbox',
+      type: 'POST',
+      data: JSON.stringify(newObj),
+      dataType: 'json',
+      contentType: 'application/x-www-form-urlencoded;charset=ISO-8859-15',
+      success: function(data) {
         $update();
-       },
-       error: function(data) {
-         // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-         console.error('chatterbox: Failed to send message');
-       }
-     });
-   });
+      },
+      error: function(data) {
+        // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+        console.error('chatterbox: Failed to send message');
+      }
+    });
+  });
 
-$('select').on('change', function() {
-  console.log($('option:selected').text());
+  $('select').on('change', function() {
+    console.log($('option:selected').text());
+  });
+
+  $('.update').click();
 });
 
-   $('.update').click();
- });
-
- var message = {
-   username: 'gerritLovesGettingHacked',
-   text: 'lets hack gerrit',
-   roomname: '4chan'
- };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+var message = {
+  username: 'gerritLovesGettingHacked',
+  text: 'lets hack gerrit',
+  roomname: '4chan'
+};
